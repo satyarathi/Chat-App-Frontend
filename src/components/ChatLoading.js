8e89510a24cd782940ac39f1474741a395edb617
DhanshreePatil01 <patildhanshrees6112@gmail.com>
2023-05-01 17:47:37 +0530

    [Dhanshree] : Added ChatPage Drawer

---
+++ src/components/ChatLoading.js
@@ -0,0 +1,19 @@
+import { Stack } from '@chakra-ui/react'
+import React from 'react'
+import { Skeleton } from '@chakra-ui/skeleton';
+
+const ChatLoading = () => {
+  return (
+    <Stack>
+     <Skeleton height='20px' />
+     <Skeleton height='20px' />
+      <Skeleton height='20px' />
+      <Skeleton height='20px' />
+      <Skeleton height='20px' />
+      <Skeleton height='20px' />
+      <Skeleton height='20px' />
+     </Stack>
+  )
+}
+
+export default ChatLoading
